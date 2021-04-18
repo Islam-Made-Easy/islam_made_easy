3acfbd2d1e1a2bdf166cba4695c4b0c6c887ba8f
kherld-hussein <eternety11@gmail.com>
2021-04-18 17:22:48 +0300

    Add && configure packages

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,12 +40,12 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar *header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "islam_made_easy");
+    gtk_header_bar_set_title(header_bar, "Islam Made Easy");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   }
   else {
-    gtk_window_set_title(window, "islam_made_easy");
+    gtk_window_set_title(window, "Islam Made Easy");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
