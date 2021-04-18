ed236dc376e7e1748f0a1954d1c06a6a500b3c49
kherld-hussein <eternety11@gmail.com>
2021-04-18 17:20:36 +0300

    Add intl assets fetch && style update

--- lib/views/prerequisite.dart
+++ lib/views/prerequisite.dart
@@ -1,24 +1,21 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:get/get.dart';
+import 'package:islam_made_easy/layout/adaptive.dart';
 import 'package:lottie/lottie.dart';
 import 'package:markdown/markdown.dart' as md;
 
-class Prerequisite extends StatefulWidget implements MarkdownDemoWidget {
+class Prerequisite extends StatefulWidget {
   @override
   _PrerequisiteState createState() => _PrerequisiteState();
-
-  @override
-  Future<String> get data async =>
-      await rootBundle.loadString('assets/md/intro.md');
 }
 
 class _PrerequisiteState extends State<Prerequisite> {
   var _extensionSet = MarkdownExtensionSet.githubFlavored;
   ScrollController controller = ScrollController();
+  String data;
 
   @override
   void initState() {
@@ -28,64 +25,89 @@ class _PrerequisiteState extends State<Prerequisite> {
 
   @override
   Widget build(BuildContext context) {
+    final isDesktop = isDisplayDesktop(context);
+    final textTheme = Theme.of(context).textTheme;
+    Locale locale = Localizations.localeOf(context);
+    preLang() {
+      if (locale.languageCode == 'ar') {
+        data = 'assets/md/intro_ar.md';
+      } else if (locale.languageCode == 'sw') {
+        data = 'assets/md/intro_sw.md';
+      } else {
+        data = 'assets/md/intro.md';
+      }
+    }
+
+    preLang();
     return Scaffold(
-      backgroundColor: Theme
-          .of(context)
-          .backgroundColor,
+      backgroundColor: Theme.of(context).backgroundColor,
       body: Scrollbar(
-        controller: controller,
         showTrackOnHover: true,
-        isAlwaysShown: true,
-        child: FutureBuilder(
-          future: widget.data,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return Column(
-                children: [
-                  SizedBox(height: 30),
-                  Expanded(
-                    child: Markdown(
-                      key: Key(_extensionSet.name),
-                      data: snapshot.data,
-                      imageDirectory: 'https://raw.githubusercontent.com',
-                      extensionSet: _extensionSet.value,
-                      styleSheet: MarkdownStyleSheet(
-                        h1: Theme
-                            .of(context)
-                            .textTheme
-                            .headline1
-                            .copyWith(
-                            fontSize: 30, fontWeight: FontWeight.bold),
-                        p: Theme
-                            .of(context)
-                            .textTheme
-                            .bodyText1,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: isDesktop ? 30 : 1, vertical: 20),
+          child: FutureBuilder(
+            future:
+                DefaultAssetBundle.of(context).loadString(data, cache: false),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return Column(
+                  children: [
+                    SizedBox(height: 30),
+                    Expanded(
+                      child: Markdown(
+                        key: Key(_extensionSet.name),
+                        data: snapshot.data,
+                        imageDirectory: 'https://raw.githubusercontent.com',
+                        extensionSet: _extensionSet.value,
+                        listItemCrossAxisAlignment:
+                            MarkdownListItemCrossAxisAlignment.start,
+                        // styleSheetTheme: MarkdownStyleSheetBaseTheme.cupertino,
+                        styleSheet: MarkdownStyleSheet(
+                          h1Align: WrapAlignment.center,
+                          h1: TextStyle(
+                              fontFamily: 'Amiri',
+                              fontWeight: FontWeight.bold,
+                              fontSize: isDesktop ? 40 : 30,
+                              letterSpacing: -3,
+                              color: Color(0xff404040)),
+                          h2: textTheme.headline6.copyWith(
+                              letterSpacing: 0.2,
+                              decoration: TextDecoration.underline,
+                              fontSize: isDesktop ? 35 : 24,
+                              fontWeight: FontWeight.bold),
+                          blockSpacing: isDesktop ? 25 : 10,
+                          textScaleFactor: 1.0,
+                          p: textTheme.bodyText1
+                              .apply(fontFamily: 'Amiri', heightDelta: 1.8),
+                        ),
+                        selectable: true,
+                        onTapLink: (text, href, title) =>
+                            linkOnTapHandler(context, text, href, title),
                       ),
-                      onTapLink: (text, href, title) =>
-                          linkOnTapHandler(context, text, href, title),
                     ),
-                  ),
-                ],
-              );
-            } else {
-              return Center(child: Lottie.asset("assets/lottie/loader.json"));
-            }
-          },
+                  ],
+                );
+              } else {
+                return Center(child: Lottie.asset("assets/lottie/loader.json"));
+              }
+            },
+          ),
         ),
       ),
     );
   }
 
-  void linkOnTapHandler(BuildContext context, String text, String href,
-      String title) async {
+  void linkOnTapHandler(
+      BuildContext context, String text, String href, String title) async {
     showDialog(
       context: context,
       builder: (context) => _createDialog(context, text, href, title),
     );
   }
 
-  Widget _createDialog(BuildContext context, String text, String href,
-      String title) =>
+  Widget _createDialog(
+          BuildContext context, String text, String href, String title) =>
       AlertDialog(
         title: Text('Reference Link'),
         content: SingleChildScrollView(
@@ -93,10 +115,7 @@ class _PrerequisiteState extends State<Prerequisite> {
             children: [
               Text(
                 'See the following link for more information:',
-                style: Theme
-                    .of(context)
-                    .textTheme
-                    .bodyText1,
+                style: Theme.of(context).textTheme.bodyText1,
               ),
               SizedBox(height: 8),
               Text('Link text: $text'),
@@ -111,17 +130,12 @@ class _PrerequisiteState extends State<Prerequisite> {
       );
 }
 
-abstract class MarkdownDemoWidget extends Widget {
-  Future<String> get data;
-}
-
 enum MarkdownExtensionSet { none, commonMark, githubFlavored, githubWeb }
 
 extension MarkdownExtensionSetExtension on MarkdownExtensionSet {
   String get name => describeEnum(this);
 
-  String get displayTitle =>
-          () {
+  String get displayTitle => () {
         switch (this) {
           case MarkdownExtensionSet.none:
             return 'None';
@@ -134,8 +148,7 @@ extension MarkdownExtensionSetExtension on MarkdownExtensionSet {
         }
       }();
 
-  md.ExtensionSet get value =>
-          () {
+  md.ExtensionSet get value => () {
         switch (this) {
           case MarkdownExtensionSet.none:
             return md.ExtensionSet.none;
@@ -152,8 +165,7 @@ extension MarkdownExtensionSetExtension on MarkdownExtensionSet {
 extension WrapAlignmentExtension on WrapAlignment {
   String get name => describeEnum(this);
 
-  String get displayTitle =>
-          () {
+  String get displayTitle => () {
         switch (this) {
           case WrapAlignment.center:
             return 'Center';
