957748a7831f62a8059dddaffd7539b2afcb84b4
kherld-hussein <eternety11@gmail.com>
2021-04-22 19:32:08 +0300

    Implement native splash and Add custom icon

--- android/app/src/main/kotlin/com/imeteam/islam_made_easy/MainActivity.kt
+++ android/app/src/main/kotlin/com/imeteam/islam_made_easy/MainActivity.kt
@@ -2,5 +2,8 @@ package com.imeteam.islam_made_easy
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 class MainActivity: FlutterActivity() {
 }
