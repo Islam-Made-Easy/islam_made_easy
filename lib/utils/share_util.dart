4041ddea756b8b10bd10abfa8cc83b3154187672
kherld-hussein <eternety11@gmail.com>
2021-05-08 18:08:10 +0300

    Separate shared strings

---
+++ lib/utils/share_util.dart
@@ -0,0 +1,19 @@
+import 'package:islam_made_easy/views/QnA/qna.dart';
+
+class ShareUtil {
+  String platformShare;
+
+  String getPlatformShare() {
+    if (DeviceOS.isWeb) {
+      platformShare = 'https://islamadeasy.page.link/share';
+    } else if (DeviceOS.isLinux) {
+      platformShare = 'https://snapcraft.io/islam-made-easy';
+    } else if (DeviceOS.isMobile) {
+      // todo: launch on PlayStore && AppStore In Sha Allah
+      platformShare = 'Apk: https://github.com/Islam-Made-Easy/Islam-Made-Easy/releases\n';
+    } else {
+      platformShare = 'https://islamadeasy.page.link/share';
+    }
+    return platformShare;
+  }
+}
