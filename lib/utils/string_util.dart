1fa290a3edc1994199d86ac6ffbd978077b3d15f
kherld-hussein <eternety11@gmail.com>
2021-06-30 01:53:40 +0300

    remove default string

--- lib/utils/string_util.dart
+++ lib/utils/string_util.dart
@@ -1,18 +1,18 @@
 class StringUtil {
   static bool isNullOrEmpty(String string) {
-    if(string == null || string.isEmpty) return true;
+    if (string.isEmpty) return true;
     else return false;
   }
   static bool isNullOrBlank(String string) {
-    if(string == null || string.trim().isEmpty) return true;
+    if (string.trim().isEmpty) return true;
     else return false;
   }
   static bool isNotEmpty(String string) {
-    if(string != null && string.isNotEmpty) return true;
+    if (string.isNotEmpty) return true;
     else return false;
   }
   static bool isNotBlank(String string) {
-    if(string != null && string.trim().isNotEmpty) return true;
+    if (string.trim().isNotEmpty) return true;
     else return false;
   }
 }
