e8ef473d5ce22948e82fef2e704928e1f92c58a8
kherld-hussein <eternety11@gmail.com>
2021-09-15 11:08:47 +0300

    Update share btn

--- lib/views/about.dart
+++ lib/views/about.dart
@@ -40,7 +40,7 @@ class _AboutAppState extends State<AboutApp> {
     final textButton = textTheme.button!.copyWith(letterSpacing: 2,fontFamily: 'Roboto');
     final name =
         'Islam Made Easy ${S.current.forPlatform} ${DeviceOS.isWeb ? 'Web' : Platform.operatingSystem.capitalizeFirst}';
-    final legalese = '© ${DateTime.now().year} The IME team';
+    final legalese = '${DateTime.now().year} The IME team';
     Locale locale = Localizations.localeOf(context);
     final ar = locale.languageCode == 'ar';
     final size = MediaQuery.of(context).size;
@@ -71,9 +71,6 @@ class _AboutAppState extends State<AboutApp> {
               'assets/images/logo.png',
               height: isDesktop ? size.height * 0.3 : 150,
             ),
-            Text(S.current.aboutApp, style: textButton.copyWith(
-                fontFamily: ar ? 'Amiri' : 'Roboto',
-                fontWeight: FontWeight.w300)),
             FutureBuilder(
               future: getVersionNumber(),
               builder: (context, snapshot) =>
@@ -88,6 +85,9 @@ class _AboutAppState extends State<AboutApp> {
                         fontSize: 28),
                   ),
             ),
+            Text(S.current.aboutApp, style: textButton.copyWith(
+                fontFamily: ar ? 'Amiri' : 'Roboto',
+                fontWeight: FontWeight.w300),textAlign: TextAlign.center),
             Text(
                 DeviceOS.isDesktop
                     ? "${Platform.operatingSystemVersion.replaceRange(23, 73, '')}"
@@ -100,60 +100,48 @@ class _AboutAppState extends State<AboutApp> {
                 child: Text(legalese, style: textButton.copyWith(
                     fontWeight: FontWeight.w400, height: 2)),
             ),
-            if (isDesktop || DeviceOS.isDesktopOrWeb)
-              Text(
+            Text(
                 '━═══◎${S.current.share}◎═══━',
                 style: TextStyle(color: colorScheme.secondary),
-              )
-            else
-              Container(),
-            if (isDesktop ||
-                DeviceOS.isDesktopOrWeb ||
-                (context.isTablet && !context.isPhone))
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ShareButtons(
-                    color: Color(0xffF59E1B),
-                    tip: 'Mail',
-                    icon: Icons.mail,
-                    onPressed: () => launchURL(
-                        'mailto:?subject=$name&body=$name ${S.current.shareText} ${ShareUtil().getPlatformShare()}'),
-                  ),
-                  ShareButtons(
-                    color: Color(0xff294C8C),
-                    tip: 'Facebook',
-                    icon: FontAwesomeIcons.facebook,
-                    onPressed: () => launchURL(
-                        'https://www.facebook.com/sharer/sharer.php?t=$name ${S.current.aboutApp}&quote=Get it from Now:&ref=fbshare&u=${ShareUtil().getPlatformShare()}'),
-                  ),
-                  ShareButtons(
-                    color: Color(0xff67C15E),
-                    tip: 'WhatsApp',
-                    icon: FontAwesomeIcons.whatsapp,
-                    onPressed: () => launchURL(
-                        'https://wa.me/?text=$name ${S.current.aboutApp}.\nGet it from Now: ${ShareUtil().getPlatformShare()}'),
-                  ),
-                  AnimatedSwitcher(
-                    duration: shareDelay.duration,
-                    child: ShareButtons(
-                        color: Color(0xffA2A2A2),
-                        tip: verified ? "Link Copied" : local.copyButtonLabel,
-                        icon: verified ? Icons.verified_user : FontAwesomeIcons.link,
-                        onPressed: () {
-                          shareDelay.run(
-                                () => Clipboard.setData(
-                                  ClipboardData(
-                                      text:
-                                      '$name ${S.current.aboutApp}.\nGet it from Now: ${ShareUtil().getPlatformShare()}'),
-                                ).then((value) {setState(() => verified = true);}),
-                          );
-                        }),
-                  ),
-                ],
-              )
-            else
-              Container()
+              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ShareButtons(
+                  tip: 'Mail',
+                  icon: FaIcon(Icons.mail,color: Color(0xffF59E1B)),
+                  onPressed: () => launchURL(
+                      'mailto:?subject=$name&body=$name ${S.current.shareText} ${ShareUtil().getPlatformShare()}'),
+                ),
+                ShareButtons(
+                  tip: 'Facebook',
+                  icon: Lottie.asset('assets/lottie/Ficon.json',height: 50,repeat: false),
+                  onPressed: () => launchURL(
+                      'https://www.facebook.com/sharer/sharer.php?t=$name ${S.current.aboutApp}&quote=Get it from Now:&ref=fbshare&u=${ShareUtil().getPlatformShare()}'),
+                ),
+                ShareButtons(
+                  tip: 'WhatsApp',
+                  icon: FaIcon(FontAwesomeIcons.whatsapp,color: Color(0xff67C15E)),
+                  onPressed: () => launchURL(
+                      'https://wa.me/?text=$name ${S.current.aboutApp}.\nGet it from Now: ${ShareUtil().getPlatformShare()}'),
+                ),
+                AnimatedSwitcher(
+                  duration: shareDelay.duration,
+                  child: ShareButtons(
+                      tip: verified ? "Link Copied" : local.copyButtonLabel,
+                      icon: FaIcon(verified?Icons.verified_user:FontAwesomeIcons.link,color: Color(0xffA2A2A2)),
+                      onPressed: () {
+                        shareDelay.run(
+                              () => Clipboard.setData(
+                            ClipboardData(
+                                text:
+                                '$name ${S.current.aboutApp}.\nGet it from Now: ${ShareUtil().getPlatformShare()}'),
+                          ).then((value) {setState(() => verified = true);}),
+                        );
+                      }),
+                ),
+              ],
+            )
           ],
         ),
       ),
