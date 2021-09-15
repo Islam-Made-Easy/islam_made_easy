0f1001ebede8532534ff6757a2126c5a133fd7d8
kherld-hussein <eternety11@gmail.com>
2021-09-15 11:12:18 +0300

    Add app commands

---
+++ lib/commands/commands.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:islam_made_easy/models/app_model.dart';
+import 'package:islam_made_easy/services/firebase_services.dart';
+import 'package:islam_made_easy/theme/themePro.dart';
+import 'package:provider/provider.dart';
+
+BuildContext? _mainContext;
+
+BuildContext get mainContext => _mainContext!;
+
+bool get hasContext => _mainContext != null;
+
+/// Someone needs to call this so our Commands can access models and services.
+void setContext(BuildContext c) => _mainContext = c;
+
+class BaseAppCommand {
+  /// Provide quick lookups for the main Models and Services in the App.
+  T getProvided<T>() {
+    assert(_mainContext != null,
+        "You must call `setContext(BuildContext)` method before calling Commands.");
+    return _mainContext!.read<T>();
+  }
+
+  ThemeProvide get appTheme => getProvided();
+  FirebaseService get firebase => getProvided();
+  AppModel get appModel => getProvided();
+}
