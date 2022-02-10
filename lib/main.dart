dd7ef0eea9e1aa85f0b509a04d0cc0b00d88b302
kherld-hussein <eternety11@gmail.com>
2022-02-10 14:54:00 +0300

    Remove breaking change 'setWindowTitle'

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:animated_background/animated_background.dart';
-import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -15,8 +14,6 @@ import 'package:islam_made_easy/models/app_model.dart';
 import 'package:islam_made_easy/routes/app_route.dart';
 import 'package:islam_made_easy/services/firebase_services.dart';
 import 'package:islam_made_easy/settings/settings_pro.dart';
-
-// import 'package:islam_made_easy/services/notification_services.dart';
 import 'package:islam_made_easy/theme/themePro.dart';
 import 'package:islam_made_easy/utils/device_info.dart';
 import 'package:islam_made_easy/utils/logger.dart';
@@ -24,16 +21,14 @@ import 'package:islam_made_easy/utils/quick_util.dart';
 import 'package:islam_made_easy/utils/sharedP.dart';
 import 'package:islam_made_easy/utils/spUtil.dart';
 import 'package:islam_made_easy/utils/string_util.dart';
-import 'package:islam_made_easy/views/home.dart';
+import 'package:islam_made_easy/views/intro/fridayRem.dart';
 import 'package:islam_made_easy/widgets/anim/load_indicator.dart';
 import 'package:provider/provider.dart';
 import 'package:url_strategy/url_strategy.dart';
-import 'package:window_size/window_size.dart';
 
 void main() async {
   initLogger(() async {
     setPathUrlStrategy();
-    // Portrait only
     // await FeedbackServices.init();
     if (kIsWeb) {
       const int megabyte = 1000000;
@@ -42,7 +37,6 @@ void main() async {
       await Future<void>.delayed(Duration.zero);
     } else if (DeviceOS.isDesktop) {
       await DesktopWindow.setMinWindowSize(const Size(1051.0, 646.0));
-      setWindowTitle('Islam Made Easy');
       // doWhenWindowReady(() {
       //   final win = appWindow;
       //   const initialSize = Size(600, 450);
@@ -153,7 +147,7 @@ class _IMEAppState extends State<IMEApp> with SingleTickerProviderStateMixin {
       onGenerateRoute: appRoute.generateRoute,
       onGenerateTitle: (context) => S.current.appTitle,
       supportedLocales: S.delegate.supportedLocales,
-      home: QuickUtil(child: Home()),
+      home: QuickUtil(child: FridayRem()),
       debugShowCheckedModeBanner: false,
     );
   }
