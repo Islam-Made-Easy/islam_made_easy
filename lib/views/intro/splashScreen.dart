600f1a2f1eb1ec38cb773b45d9c17957a651f667
kherld-hussein <eternety11@gmail.com>
2022-12-10 14:08:06 +0300

    Refactor code

--- lib/views/intro/splashScreen.dart
+++ lib/views/intro/splashScreen.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import 'package:islam_made_easy/utils/load_util.dart';
 import 'package:islam_made_easy/views/home.dart';
 import 'package:islam_made_easy/views/intro/fridayRem.dart';
-import 'package:islam_made_easy/widgets/anim/load_indicator.dart';
+import 'package:shimmer/shimmer.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -15,13 +15,7 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> with LoadingStateMixin {
   bool isCelebration = false;
 
-  Future handleStartUpLogic() async {
-    // bool success =
-    //     await load(() async => await AuthenticateUserCommand().run());
-    // if (success) {
-    //  setState(() {getNav();});
-    // }
-  }
+  Future handleStartUpLogic() async {}
 
   getNav() {
     isCelebration ? Get.off(() => FridayRem()) : Get.off(() => Home());
@@ -32,11 +26,30 @@ class _SplashScreenState extends State<SplashScreen> with LoadingStateMixin {
     super.initState();
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-    // handleStartUpLogic();
-    return Scaffold(body: LoadingIndicator());
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(
+            child: Shimmer.fromColors(
+              baseColor: Color(0xffe2cf84),
+              highlightColor: Get.theme.indicatorColor,
+              child: Text(
+                'ISLAM MADE EASY',
+                style: TextStyle(
+                  fontSize: 60,
+                  fontFamily: 'Quicksand',
+                  color: Color(0xffe2cf84),
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Image.asset('assets/images/up.png', height: 600),
+          ),
+        ],
+      ),
+    );
   }
 }
