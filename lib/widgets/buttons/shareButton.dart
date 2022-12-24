64df09755071ce9201b9c4670dfb3257b65e2cfe
kherld-hussein <eternety11@gmail.com>
2022-12-24 22:11:39 +0300

    Refactor: - Remove InkWell - Add IconButton - Add decoration to the tooltip - Reformat code

--- lib/widgets/buttons/shareButton.dart
+++ lib/widgets/buttons/shareButton.dart
@@ -2,30 +2,69 @@ import 'package:islam_made_easy/views/QnA/qna.dart';
 
 class ShareButtons extends StatelessWidget {
   final String? tip;
-  final Widget? icon;
-  final Function? onPressed;
+  final IconData? icon;
+  final void Function() onPressed;
   final Color? color;
 
-  const ShareButtons({Key? key, this.color, this.icon, this.onPressed, this.tip}) : super(key: key);
+  const ShareButtons(
+      {Key? key, this.color, this.icon, required this.onPressed, this.tip})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final isDesktop = isDisplayDesktop(context);
     return Container(
-      padding: EdgeInsets.all(isDesktop?10:0),
+      padding: EdgeInsets.all(isDesktop ? 10 : 0),
       decoration: BoxDecoration(
         shape: BoxShape.circle,
         boxShadow: [
-          BoxShadow(color: Colors.grey[300]!, offset: Offset(4.0, 4.0), blurRadius: isDesktop?10:2, spreadRadius: 1.0),
-          BoxShadow(color: color!.withOpacity(.09), offset: Offset(-4.0, -4.0), blurRadius:  isDesktop?10:2, spreadRadius: 1.0),
-          BoxShadow(color: Colors.transparent.withOpacity(.09), offset: Offset(-4.0, -4.0), blurRadius:  isDesktop?10:2, spreadRadius: 1.0),
+          BoxShadow(
+            color: Colors.grey[300]!,
+            offset: Offset(4.0, 4.0),
+            blurRadius: isDesktop ? 10 : 2,
+            spreadRadius: 1.0,
+          ),
+          BoxShadow(
+            color: color!.withOpacity(.09),
+            offset: Offset(-4.0, -4.0),
+            blurRadius: isDesktop ? 10 : 2,
+            spreadRadius: 1.0,
+          ),
+          BoxShadow(
+            color: Colors.transparent.withOpacity(.09),
+            offset: Offset(-4.0, -4.0),
+            blurRadius: isDesktop ? 10 : 2,
+            spreadRadius: 1.0,
+          ),
         ],
         gradient: gradient,
       ),
-      child: InkWell(child: Tooltip(message: tip!,child: icon,textStyle: Theme.of(context).textTheme.button!.copyWith(fontFamily: 'Roboto',fontWeight: FontWeight.w100)),onTap: onPressed as void Function()?),
+      child: Tooltip(
+        message: tip!,
+        decoration: BoxDecoration(
+          color: color!.withOpacity(.4),
+          borderRadius: BorderRadius.all(Radius.circular(isDesktop ? 14 : 7)),
+        ),
+        child: IconButton(
+          icon: FaIcon(icon!),
+          splashRadius: isDesktop ? 40 : 20,
+          hoverColor: color!.withOpacity(.04),
+          color: color,
+          onPressed: onPressed,
+        ),
+      ),
     );
   }
 }
+
 LinearGradient get gradient => LinearGradient(colors: [
-  Colors.grey[100]!, Colors.grey[200]!, Colors.grey[300]!, Colors.grey[400]!,
-], stops: [.0, .3, .8, 1.0], begin: Alignment.bottomLeft, end: Alignment.topRight);
+      Colors.grey[100]!,
+      Colors.grey[200]!,
+      Colors.grey[300]!,
+      Colors.grey[400]!,
+    ], stops: [
+      .0,
+      .3,
+      .8,
+      1.0,
+    ], begin: Alignment.topLeft, end: Alignment.bottomLeft);
