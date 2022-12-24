bcf607bae5f9f778ebb86d4b57c11796aadaac3e
kherld-hussein <eternety11@gmail.com>
2022-12-24 22:03:12 +0300

    Update: - Remove deprecated url_launcher features - Restore ShareButtons - Refactor

--- lib/views/about.dart
+++ lib/views/about.dart
@@ -37,7 +37,7 @@ class _AboutAppState extends State<AboutApp> {
     final textButton =
         textTheme.button!.copyWith(letterSpacing: 2, fontFamily: 'Roboto');
     final name =
-        'Islam Made Easy ${S.current.forPlatform} ${DeviceOS.isWeb ? 'Web' : Platform.operatingSystem.capitalizeFirst}';
+        'Islam Made Easy ${S.current.forPlatform} ${DeviceOS.isWeb ? 'Web' : Platform.operatingSystem}';
     final legalese = '${DateTime.now().year} The IME team';
     Locale locale = Localizations.localeOf(context);
     final ar = locale.languageCode == 'ar';
@@ -46,9 +46,11 @@ class _AboutAppState extends State<AboutApp> {
       title: ListHeader(
         text: local.aboutListTileTitle('IME'),
         trailing: IconButton(
-          icon: FaIcon(DeviceOS.isMobile
-              ? FontAwesomeIcons.shareAlt
-              : FontAwesomeIcons.timesCircle),
+          icon: FaIcon(
+            DeviceOS.isMobile
+                ? FontAwesomeIcons.shareAlt
+                : FontAwesomeIcons.timesCircle,
+          ),
           onPressed: () {
             DeviceOS.isDesktopOrWeb
                 ? Get.back()
@@ -66,9 +68,9 @@ class _AboutAppState extends State<AboutApp> {
         child: Column(
           children: [
             Image.asset(
-              'assets/images/1.png',
+              'assets/images/divi.png',
+              scale: 3,
               color: Theme.of(context).primaryColor.withOpacity(.5),
-              width: 350,
             ),
             Image.asset(
               'assets/images/Logo3.png',
@@ -82,75 +84,91 @@ class _AboutAppState extends State<AboutApp> {
                     : '$name',
                 textAlign: TextAlign.center,
                 style: textTheme.caption!.copyWith(
-                    letterSpacing: .5,
-                    fontFamily: 'Quicksand',
-                    fontWeight: FontWeight.w500,
-                    fontSize: 28),
+                  letterSpacing: .5,
+                  fontFamily: 'Quicksand',
+                  fontWeight: FontWeight.w500,
+                  fontSize: 28,
+                ),
               ),
             ),
-            Text(S.current.aboutApp,
-                style: textButton.copyWith(
-                    fontFamily: ar ? 'Amiri' : 'Roboto',
-                    fontWeight: FontWeight.w300),
-                textAlign: TextAlign.center),
+            Text(
+              S.current.aboutApp,
+              style: textButton.copyWith(
+                fontFamily: ar ? 'Amiri' : 'Roboto',
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.center,
+            ),
             Shimmer.fromColors(
               highlightColor: colorScheme.onBackground,
               loop: 2,
               baseColor: colorScheme.primary,
               child: GestureDetector(
-                onTap: () {
-                  launchURL('https://github.com/Islam-Made-Easy/');
-                },
-                child: Text(legalese,
-                    style: textButton.copyWith(
-                        fontWeight: FontWeight.w400,
-                        height: 2,
-                        decoration: TextDecoration.underline,
-                        fontFamily: 'Quicksand')),
+                onTap: () => launchURL('https://github.com/Islam-Made-Easy/'),
+                child: Text(
+                  legalese,
+                  style: textButton.copyWith(
+                    fontWeight: FontWeight.w400,
+                    height: 2,
+                    decoration: TextDecoration.underline,
+                    fontFamily: 'Quicksand',
+                  ),
+                ),
               ),
             ),
             Text(
               '━═══◎${S.current.share}◎═══━',
               style: TextStyle(color: colorScheme.secondary),
             ),
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            //   children: [
-            //     ShareButtons(
-            //       tip: 'Mail',
-            //       icon: FaIcon(Icons.mail,color: Color(0xffF59E1B)),
-            //       onPressed: () => launchURL(
-            //           'mailto:?subject=$name&body=$name ${S.current.shareText} ${ShareUtil().getPlatformShare()}'),
-            //     ),
-            //     ShareButtons(
-            //       tip: 'Facebook',
-            //       icon: Lottie.asset('assets/lottie/Ficon.json',height: 50,repeat: false),
-            //       onPressed: () => launchURL(
-            //           'https://www.facebook.com/sharer/sharer.php?t=$name ${S.current.aboutApp}&quote=Get it from Now:&ref=fbshare&u=${ShareUtil().getPlatformShare()}'),
-            //     ),
-            //     ShareButtons(
-            //       tip: 'WhatsApp',
-            //       icon: FaIcon(FontAwesomeIcons.whatsapp,color: Color(0xff67C15E)),
-            //       onPressed: () => launchURL(
-            //           'https://wa.me/?text=$name ${S.current.aboutApp}.\nGet it from Now: ${ShareUtil().getPlatformShare()}'),
-            //     ),
-            //     AnimatedSwitcher(
-            //       duration: Duration(milliseconds: 200),
-            //       child: ShareButtons(
-            //           tip: verified ? "Link Copied" : local.copyButtonLabel,
-            //           icon: FaIcon(verified?Icons.verified_user:FontAwesomeIcons.link,color: Color(0xffA2A2A2)),
-            //           onPressed: () {
-            //             shareDelay.run(
-            //                   () => Clipboard.setData(
-            //                 ClipboardData(
-            //                     text:
-            //                     '$name ${S.current.aboutApp}.\nGet it from Now: ${ShareUtil().getPlatformShare()}'),
-            //               ).then((value) {setState(() => verified = true);}),
-            //             );
-            //           }),
-            //     ),
-            //   ],
-            // )
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ShareButtons(
+                  onPressed: () => launchURL(
+                      'mailto:?subject=$name&body=$name ${S.current.shareText} ${ShareUtil().getPlatformShare()}'),
+                  tip: 'Mail',
+                  color: Color(0xffF59E1B),
+                  icon: Icons.mail,
+                ),
+                ShareButtons(
+                  onPressed: () => launchURL(
+                      'https://www.facebook.com/sharer/sharer.php?t=$name ${S.current.aboutApp}&quote=Get it from Now:&ref=fbshare&u=${ShareUtil().getPlatformShare()}'),
+                  tip: 'Facebook',
+                  color: Color(0xff2114BE),
+                  icon: FontAwesomeIcons.facebook,
+                ),
+                ShareButtons(
+                  onPressed: () => launchURL(
+                      'https://wa.me/?text=$name ${S.current.aboutApp}.\nGet it from Now: ${ShareUtil().getPlatformShare()}'),
+                  tip: 'WhatsApp',
+                  color: Color(0xff11C11E),
+                  icon: FontAwesomeIcons.whatsapp,
+                ),
+                ShareButtons(
+                  onPressed: () {
+                    shareDelay.run(
+                      () => Clipboard.setData(
+                        ClipboardData(
+                            text:
+                                '$name ${S.current.aboutApp}.\nGet it from Now: ${ShareUtil().getPlatformShare()}'),
+                      ).then((value) {
+                        setState(() => verified = true);
+                      }),
+                    );
+                  },
+                  color: Color(0xFF666666),
+                  tip: verified ? "Link Copied" : local.copyButtonLabel,
+                  icon: verified
+                      ? Icons.verified_user
+                      : FontAwesomeIcons.link,
+                ),
+              ],
+            ),
+            Image.asset(
+              'assets/images/1.png',
+              scale: 3,
+              color: Theme.of(context).primaryColor.withOpacity(.5),
+            )
           ],
         ),
       ),
@@ -159,8 +177,8 @@ class _AboutAppState extends State<AboutApp> {
 }
 
 Future launchURL(String url) async {
-  if (await canLaunch(url)) {
-    await launch(url);
+  if (await canLaunchUrl(Uri.parse(url))) {
+    await launchUrl(Uri.parse(url));
   } else {
     throw 'Could not launch $url';
   }
