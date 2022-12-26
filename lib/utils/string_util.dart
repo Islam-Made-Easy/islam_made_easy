2f5c09b7714ee138083cddaf788ca1ee72c9c0d5
kherld-hussein <eternety11@gmail.com>
2022-12-26 15:56:12 +0300

    Reformat code

--- lib/utils/string_util.dart
+++ lib/utils/string_util.dart
@@ -1,18 +1,29 @@
 class StringUtil {
   static bool isNullOrEmpty(String string) {
-    if (string.isEmpty) return true;
-    else return false;
+    if (string.isEmpty)
+      return true;
+    else
+      return false;
   }
+
   static bool isNullOrBlank(String string) {
-    if (string.trim().isEmpty) return true;
-    else return false;
+    if (string.trim().isEmpty)
+      return true;
+    else
+      return false;
   }
+
   static bool isNotEmpty(String string) {
-    if (string.isNotEmpty) return true;
-    else return false;
+    if (string.isNotEmpty)
+      return true;
+    else
+      return false;
   }
+
   static bool isNotBlank(String string) {
-    if (string.trim().isNotEmpty) return true;
-    else return false;
+    if (string.trim().isNotEmpty)
+      return true;
+    else
+      return false;
   }
 }
