0050194224103e490bf5efa1eeaa8aab5136ae09
kherld-hussein <eternety11@gmail.com>
2022-12-26 15:56:45 +0300

    Reformat code

--- lib/utils/spUtil.dart
+++ lib/utils/spUtil.dart
@@ -7,16 +7,34 @@ class SpUtil {
   static String fontFamily = 'fontFamily';
   static String fontSize = 'fontSize';
   static String fullScreen = 'fullScreen';
+
   static int? getThemeIndex() => appSP.getInt(themeIndex);
+
   static bool? getDarkTheme() => appSP.getBool(isDarkMode);
-  static Future<bool> setThemeIndex(int value) => appSP.setInt(themeIndex, value);
+
+  static Future<bool> setThemeIndex(int value) =>
+      appSP.setInt(themeIndex, value);
+
   static String? getLanguage() => appSP.getString(language);
+
   static String? getFont() => appSP.getString(fontFamily);
+
   static double? getFontSize() => appSP.getDouble(fontSize);
+
   static bool? getFullScreen() => appSP.getBool(fullScreen);
-  static Future<bool> setLanguage(String value) => appSP.setString(language, value);
-  static Future<bool> setFullScreen(bool value) => appSP.setBool(fullScreen, value);
-  static Future<bool> setDarkTheme(bool value) => appSP.setBool(isDarkMode, value);
-  static Future<bool> setFont(String value) => appSP.setString(fontFamily, value);
-  static Future<bool> setFontSize(double value) => appSP.setDouble(fontSize, value);
+
+  static Future<bool> setLanguage(String value) =>
+      appSP.setString(language, value);
+
+  static Future<bool> setFullScreen(bool value) =>
+      appSP.setBool(fullScreen, value);
+
+  static Future<bool> setDarkTheme(bool value) =>
+      appSP.setBool(isDarkMode, value);
+
+  static Future<bool> setFont(String value) =>
+      appSP.setString(fontFamily, value);
+
+  static Future<bool> setFontSize(double value) =>
+      appSP.setDouble(fontSize, value);
 }
