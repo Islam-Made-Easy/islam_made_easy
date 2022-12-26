af36a21eb7a962c285f9d189a46101eec50e005f
kherld-hussein <eternety11@gmail.com>
2022-12-26 18:53:21 +0300

    Reformat code

--- lib/widgets/anim/fade_slide.dart
+++ lib/widgets/anim/fade_slide.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class FadingSlidingWidget extends StatelessWidget {
-  const FadingSlidingWidget(
-      {AnimationController? anim, this.child, this.interval = const Interval(0.5, 1.0)})
-      : _anim = anim;
+  const FadingSlidingWidget({
+    AnimationController? anim,
+    this.child,
+    this.interval = const Interval(.5, 1.0),
+  }) : _anim = anim;
 
   final AnimationController? _anim;
   final Widget? child;
@@ -13,11 +15,12 @@ class FadingSlidingWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return SlideTransition(
       position: _anim!.drive(
-        Tween<Offset>(begin: Offset(0, 0.5), end: Offset.zero).chain(CurveTween(curve: interval)),
+        Tween<Offset>(begin: Offset(0, .5), end: Offset.zero)
+            .chain(CurveTween(curve: interval)),
       ),
       child: FadeTransition(
         opacity: _anim!.drive(
-          Tween<double>(begin: 0.0, end: 1.0).chain(CurveTween(curve: interval)),
+          Tween<double>(begin: .0, end: 1.0).chain(CurveTween(curve: interval)),
         ),
         child: child,
       ),
