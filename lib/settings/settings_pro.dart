a2f29ee0882e7b3e883b9503613c3814891cc1f6
kherld-hussein <eternety11@gmail.com>
2022-12-26 16:04:25 +0300

    Merge locale provider module to settings provider - Enhances code readability and easy debug of app settings.

--- lib/settings/settings_pro.dart
+++ lib/settings/settings_pro.dart
@@ -2,9 +2,14 @@ import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
 import 'package:islam_made_easy/utils/spUtil.dart';
 
+import '../theme/themePro.dart';
+
 class SettingProvide with ChangeNotifier {
   String? _fontType = 'Roboto';
   double? _fontSize = 16;
+  Locale? _locale;
+
+  Locale? get locale => _locale;
 
   getFontSize(double fontSize) {
     _fontSize = fontSize;
@@ -25,6 +30,17 @@ class SettingProvide with ChangeNotifier {
   void getFullScreen(bool full) async {
     await DesktopWindow.setFullScreen(full);
     SpUtil.setFullScreen(full);
+    notifyListeners();
+  }
+
+  changeLocale(Locale? locale) {
+    _locale = locale;
+    notifyListeners();
+  }
+
+  void getDark(bool dark) {
+    ThemeProvide().changeDark(dark);
+    SpUtil.setDarkTheme(dark);
     notifyListeners();
   }
 }
