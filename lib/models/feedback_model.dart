cc67c3ee1ce8366229a5f44a2a0d0fe869a26dc3
kherld-hussein <eternety11@gmail.com>
2022-12-26 18:58:15 +0300

    Reformat code

--- lib/models/feedback_model.dart
+++ lib/models/feedback_model.dart
@@ -5,19 +5,23 @@ class FeedbackModel {
   const FeedbackModel({this.name, this.email, this.feedback});
 
   factory FeedbackModel.fromJson(dynamic json) {
-    return FeedbackModel(name:"${json['name']}",email: "${json['email']}",feedback: "${json['feedback']}");
+    return FeedbackModel(
+      name: "${json['name']}",
+      email: "${json['email']}",
+      feedback: "${json['feedback']}",
+    );
   }
 
   // Method to make GET parameters.
-  Map<String,dynamic> toJson() => {
-    FeedbackFields.name: name,
-    FeedbackFields.email: email,
-    FeedbackFields.feedback: feedback
-  };
+  Map<String, dynamic> toJson() => {
+        FeedbackFields.name: name,
+        FeedbackFields.email: email,
+        FeedbackFields.feedback: feedback
+      };
 }
 
-class FeedbackFields{
+class FeedbackFields {
   static final String name = 'name', email = 'email', feedback = 'feedback';
 
-  static List<String> getFields()=>[name,email,feedback];
+  static List<String> getFields() => [name, email, feedback];
 }
