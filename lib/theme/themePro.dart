0a5d42ee798c9e8cccd9a15d4bc0bdf95e309536
kherld-hussein <eternety11@gmail.com>
2022-12-31 06:01:58 +0300

    Update Theme

--- lib/theme/themePro.dart
+++ lib/theme/themePro.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:islam_made_easy/utils/spUtil.dart';
 
 const kSpacingUnit = 10;
 const kLightSecondaryColor = Color(0xfff4f6f9);
@@ -8,8 +7,8 @@ const kDarkSecondaryColor = Color(0xFF272042);
 const kLightPrimaryColor = Color(0xFFFFFFFF);
 const kDarkPrimaryColor = Color(0xff2934BE);
 const kDarkPrimaryColor2 = Color(0xff0e1621);
-const kBackgroundLight = Color(0xFFF2F3F7);
-const kBackgroundDark = Color(0xFF17212B);
+const kBackgroundLight = Color(0xfff0eeef);
+const kBackgroundDark = Color(0xFF11151E);
 const kDividerLight = Color(0xFFFFFFFF);
 const kDividerDark = Color(0xFF444444);
 const kAccentColor = Color(0x95AC25FF);
@@ -21,14 +20,16 @@ const kDarkColor = Color(0xFF202A43);
 const kIconLight = Color(0xFFFBFBFB);
 const kIconDark = Color(0xFF666666);
 const kGreen = Color(0xFFBCFF00);
+const kGreenLight = Color(0xFFD6FC51);
+const kColorG = Color(0xffffe598);
 const kColor = Color(0xff0f1f27);
 const kLight = Color(0xFFFDFDFD);
-const kDark = Color(0xFF1A2C33);
+const kDark = Color(0xFF151D2A);
 const kGreyColor = Color(0xffBBC3D7);
 
 class ThemeProvide with ChangeNotifier {
   // To make the app standard start with index 5
-  static const DEFAULT_THEME_INDEX = 5;
+  static const DEFAULT_THEME_INDEX = 1;
   int _themeIndex = DEFAULT_THEME_INDEX;
 
   int get themeIndex => _themeIndex;
@@ -75,60 +76,89 @@ class ThemeProvide with ChangeNotifier {
     if (themeIndex >= 0 && themeIndex < Colors.primaries.length) {
       this._themeIndex = themeIndex;
       this._themeData = ThemeData(
-          fontFamily: 'Amiri',
-          splashFactory: InkRipple.splashFactory,
-          appBarTheme: AppBarTheme(elevation: 0),
-          scaffoldBackgroundColor: kBackgroundLight,
-          backgroundColor: kBackgroundLight,
-          primarySwatch: Colors.primaries[themeIndex],
-          textTheme: TextTheme(
-            bodyText1: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 16.0,
-              fontFamily: 'Amiri',
-            ),
-            headline5: TextStyle(
-              fontWeight: FontWeight.w700,
-              letterSpacing: .5,
-              fontSize: 20,
-              fontFamily: 'Amiri',
-            ),
-            headline6: TextStyle(
-              fontWeight: FontWeight.w500,
-              letterSpacing: .5,
-              fontSize: 20,
-              fontFamily: 'Amiri',
-            ),
-            subtitle1: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 16.0,
-              fontFamily: 'Amiri',
-            ),
+        fontFamily: 'Amiri',
+        splashFactory: InkRipple.splashFactory,
+        appBarTheme: AppBarTheme(elevation: 0),
+        scaffoldBackgroundColor: kBackgroundLight,
+        backgroundColor: kBackgroundLight,
+        primarySwatch: Colors.primaries[themeIndex],
+        tooltipTheme: TooltipThemeData(
+          decoration: BoxDecoration(
+            color: Colors.primaries[themeIndex].withOpacity(.4),
+            borderRadius: BorderRadius.all(Radius.circular(14)),
           ),
-          cardColor: kLightSecondaryColor,
-          cardTheme: CardTheme(
-            clipBehavior: Clip.antiAlias,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          textStyle: TextStyle(color: Colors.white, fontFamily: 'Quicksand'),
+        ),
+        splashColor: Colors.primaries[themeIndex].withOpacity(.05),
+        textTheme: TextTheme(
+          bodyText1: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 16.0,
+            fontFamily: 'Amiri',
           ),
-          textButtonTheme: TextButtonThemeData(
-              style: ElevatedButton.styleFrom(
-                  textStyle: TextStyle(
-                      fontSize: kSpacingUnit * 1.3,
-                      fontWeight: FontWeight.w100,
-                      letterSpacing: 2))),
-          textSelectionTheme:
-              TextSelectionThemeData(cursorColor: Colors.primaries[themeIndex]),
-          navigationRailTheme: NavigationRailThemeData(
-            backgroundColor: Color(0xFFF2F2F2),
-            labelType: NavigationRailLabelType.none,
-          ));
+          headline5: TextStyle(
+            fontWeight: FontWeight.w700,
+            letterSpacing: .5,
+            fontSize: 20,
+            fontFamily: 'Amiri',
+          ),
+          headline6: TextStyle(
+            fontWeight: FontWeight.w500,
+            letterSpacing: .5,
+            fontSize: 20,
+            fontFamily: 'Amiri',
+          ),
+          subtitle1: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 16.0,
+            fontFamily: 'Amiri',
+          ),
+        ),
+        cardColor: kLightSecondaryColor,
+        cardTheme: CardTheme(
+          clipBehavior: Clip.antiAlias,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        ),
+        textButtonTheme: TextButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                textStyle: TextStyle(
+                    fontSize: kSpacingUnit * 1.3,
+                    fontWeight: FontWeight.w100,
+                    letterSpacing: 2))),
+        textSelectionTheme:
+            TextSelectionThemeData(cursorColor: Colors.primaries[themeIndex]),
+        navigationRailTheme: NavigationRailThemeData(
+          backgroundColor: Color(0xFFF2F2F2),
+          labelType: NavigationRailLabelType.none,
+          selectedLabelTextStyle: TextStyle(fontWeight: FontWeight.w900),
+        ),
+      );
       this._themeDataDark = ThemeData(
           brightness: Brightness.dark,
           fontFamily: 'Amiri',
           splashFactory: InkRipple.splashFactory,
           appBarTheme: AppBarTheme(elevation: 0),
           accentColor: Colors.primaries[themeIndex],
+          tooltipTheme: TooltipThemeData(
+            decoration: BoxDecoration(
+              color: Colors.primaries[themeIndex].withOpacity(.4),
+              borderRadius: BorderRadius.all(Radius.circular(14)),
+            ),
+            textStyle: TextStyle(color: Colors.white),
+          ),
+          splashColor: Colors.primaries[themeIndex].withOpacity(.05),
+          // colorScheme: ColorScheme(
+          //     brightness: Brightness.dark,
+          //     primary: Colors.primaries[themeIndex],
+          //     onPrimary: Colors.primaries[themeIndex],
+          //     secondary: Colors.primaries[themeIndex],
+          //     onSecondary: Colors.primaries[themeIndex],
+          //     error: themeData.errorColor,
+          //     onError: themeData.errorColor,
+          //     background: kBackgroundDark,
+          //     onBackground: kBackgroundDark,
+          //     surface: Colors.primaries[themeIndex],
+          //     onSurface: Colors.primaries[themeIndex]),
           dialogTheme: DialogTheme(backgroundColor: kDark),
           primaryColor: Colors.primaries[themeIndex],
           primarySwatch: Colors.primaries[themeIndex],
@@ -174,10 +204,11 @@ class ThemeProvide with ChangeNotifier {
           primaryColorDark: kDarkPrimaryColor2,
           scaffoldBackgroundColor: kBackgroundDark,
           backgroundColor: kDarkSecondaryColor,
-          dividerColor: kDividerLight,
+          dividerColor: Colors.primaries[themeIndex].withOpacity(.05),
           navigationRailTheme: NavigationRailThemeData(
             backgroundColor: kDark,
             labelType: NavigationRailLabelType.none,
+            selectedLabelTextStyle: TextStyle(fontWeight: FontWeight.w900),
           ));
       _overlayStyle = SystemUiOverlayStyle.light.copyWith(
         statusBarColor: Colors.primaries[themeIndex],
@@ -197,12 +228,6 @@ class ThemeProvide with ChangeNotifier {
 
   void changeDark(bool isdarkmode) {
     isDarkMode = isdarkmode;
-    notifyListeners();
-  }
-
-  void getDark(bool dark) {
-    ThemeProvide().changeDark(dark);
-    SpUtil.setDarkTheme(dark);
     notifyListeners();
   }
 }
